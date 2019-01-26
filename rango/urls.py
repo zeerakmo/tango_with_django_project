eaa8aeefa671faed3146d9a0932254f7f50eb4ca
Zeerak Mohammed <2314940z@student.gla.ac.uk>
2019-01-26 15:56:08 +0000

    6th Commit - Completed Chapter 6

--- rango/urls.py
+++ rango/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from rango import views
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'^about/$', views.about, name='about'),
+        url(r'^$', views.index, name='index'),
+        url(r'^about/$', views.about, name='about'),
+       
+        url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+            views.show_category, name='show_category'),
+
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,20 +1,44 @@
 from django.shortcuts import render
-
+from rango.models import Category
+from rango.models import Page
 # Create your views here.
 from django.http import HttpResponse
+
+def show_category(request, category_name_slug):
+        context_dict = {}
+
+        try:
+                category = Category.objects.get(slug=category_name_slug)
+                pages = Page.objects.filter(category=category)
+
+                ##context_dict['category_name'] = category.name
+
+                
+                context_dict['pages'] = pages
+                context_dict['category'] = category
+        except Category.DoesNotExist:
+                context_dict['category'] = None
+                context_dict['pages'] = None
+
+                
+                
+        return render(request, 'rango/category.html', context_dict)
+
+
+
+
+
+
+
 def index(request):
-	# Construct a dictionary to pass to the template engine as its context.
-	# Note the key boldmessage is the same as {{ boldmessage }} in the template!
-	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-	# Return a rendered response to send to the client.
-	# We make use of the shortcut function to make our lives easier.
-	# Note that the first parameter is the template we wish to use.
-	return render(request, 'rango/index.html', context=context_dict)
+
+        category_list = Category.objects.order_by('-likes')[:5]
+        
+
+        page_list = Page.objects.order_by('-views')[:5]
+        context_dict = {'categories': category_list, 'pages': page_list}
+        
+        return render(request, 'rango/index.html', context_dict)
 def about(request):
-	# Construct a dictionary to pass to the template engine as its context.
-	# Note the key boldmessage is the same as {{ boldmessage }} in the template!
-	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-	# Return a rendered response to send to the client.
-	# We make use of the shortcut function to make our lives easier.
-	# Note that the first parameter is the template we wish to use.
-	return render(request, 'rango/about.html', context=context_dict)	
+        context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+        return render(request, 'rango/about.html', context=context_dict)	
