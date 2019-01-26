cd704f1288de179c7f17fb8efcc8e6706cf0248b
Zeerak Mohammed <2314940z@student.gla.ac.uk>
2019-01-26 13:12:34 +0000

    5th Commit - Chapter 5

--- rango/models.py
+++ rango/models.py
@@ -6,13 +6,13 @@ class Category(models.Model):
 	views = models.IntegerField(default=0)
 	likes = models.IntegerField(default=0)
 	class Meta:
-		verbose_name_plural = 'Categories'
-def __str__(self): # For Python 2, use __unicode__ too
-	return self.name
+                        verbose_name_plural = 'Categories'
+	def __str__(self):
+                        return self.name
 class Page(models.Model):
 	category = models.ForeignKey(Category)
 	title = models.CharField(max_length=128)
 	url = models.URLField()
 	views = models.IntegerField(default=0)
-def __str__(self): # For Python 2, use __unicode__ too
-	return self.title
+	def __str__(self):
+                        return self.title
